6e515744fccb1fb363d3bf9ffec7d372912a1842
Johan <johan.chapelain@gmail.com>
2021-06-12 17:25:57 +0200

    changes

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='meteoblue_wrapper',
+    name='meteojo',
     version='0.0.1',
-    url='https://github.com/j0hanj0han/meteoblue_wrapper.git',
+    url='https://github.com/j0hanj0han/meteojo.git',
     author='j0hanj0han',
     author_email='author@gmail.com',
     description='get open weather data easily',
